a51c979b78bdf7bcab8077ec626f523de6ffce0e
Pink Sammy <shoujo@gmail.com>
2021-08-07 14:21:01 +0100

    adding functions to roll on all tables.  added functions for all tiers.

--- mainDice.js
+++ mainDice.js
@@ -33,35 +33,192 @@ function treasureHoardTier1() {
     rollOnTableG()
     return
 }
+function treasureHoardTier2() {
+    var diceRoll = diceRollD100();
+    console.log("initial hoard roll: ", diceRoll);
+    if (diceRoll <= 28) {
+        console.log("Nothing!")
+        return
+    }
+    if (diceRoll <= 44) {
+        var diceRoll6 = diceRollD6();
+        console.log("Roll " + diceRoll6 + " times on Magic Item Table A")
+        rollOnTableA(diceRoll6)
+        return
+    }
+    if (diceRoll <= 63) {
+        var diceRoll4 = diceRollD4();
+        console.log("Roll " + diceRoll4 + " times on Magic Item Table B")
+        rollOnTableB(diceRoll4)
+        return
+    }
+    if (diceRoll <= 74) {
+        var diceRoll4 = diceRollD4();
+        console.log("Roll " + diceRoll4 + " times on Magic Item Table C")
+        rollOnTableC(diceRoll4)
+        return
+    }
+    if (diceRoll <= 80) {
+        console.log("Roll once on Magic Item Table D")
+        rollOnTableD()
+        return
+    }
+    if (diceRoll <= 94) {
+        var diceRoll4 = diceRollD4();
+        console.log("Roll " + diceRoll4 + " times on Magic Item Table F")
+        rollOnTableF(diceRoll4)
+        return
+    }
+    if (diceRoll <= 98) {
+        var diceRoll4 = diceRollD4();
+        console.log("Roll " + diceRoll4 + " times on Magic Item Table G")
+        rollOnTableG(diceRoll4)
+        return
+    }
+    console.log("Roll once on Magic Item Table H")
+    rollOnTableH()
+    return
+}
+function treasureHoardTier3() {
+    var diceRoll = diceRollD100();
+    console.log("initial hoard roll: ", diceRoll);
+    if (diceRoll <= 15) {
+        console.log("Nothing!")
+        return
+    }
+    if (diceRoll <= 29) {
+        var diceRoll4 = diceRollD4();
+        var diceRoll6 = diceRollD6();
+        console.log("Roll " + diceRoll4 + " times on Magic Item Table A & " + diceRoll6 + " times on Magic Item Table B")
+        rollOnTableA(diceRoll4)
+        rollOnTableB(diceRoll6)
+        return
+    }
+    if (diceRoll <= 50) {
+        var diceRoll6 = diceRollD6();
+        console.log("Roll " + diceRoll6 + " times on Magic Item Table C")
+        rollOnTableC(diceRoll6)
+        return
+    }
+    if (diceRoll <= 66) {
+        var diceRoll4 = diceRollD4();
+        console.log("Roll " + diceRoll4 + " times on Magic Item Table D")
+        rollOnTableD(diceRoll4)
+        return
+    }
+    if (diceRoll <= 74) {
+        console.log("Roll once on Magic Item Table E")
+        rollOnTableE()
+        return
+    }
+    if (diceRoll <= 82) {
+        var diceRoll4 = diceRollD4();
+        console.log("Roll once on Magic Item Table F & " + diceRoll4 + " times on Magic Item Table G")
+        rollOnTableF()
+        rollOnTableG(diceRoll4)
+        return
+    }
+    if (diceRoll <= 92) {
+        var diceRoll4 = diceRollD4();
+        console.log("Roll " + diceRoll4 + " times on Magic Item Table H")
+        rollOnTableH(diceRoll4)
+        return
+    }
+    console.log("Roll once on Magic Item Table I")
+    rollOnTableI()
+    return
+}
+function treasureHoardTier4() {
+    var diceRoll = diceRollD100();
+    console.log("initial hoard roll: ", diceRoll);
+    if (diceRoll <= 2) {
+        console.log("Nothing!")
+        return
+    }
+    if (diceRoll <= 14) {
+        var diceRoll8 = diceRollD8();
+        console.log("Roll " + diceRoll8 + " times on Magic Item Table C")
+        rollOnTableA(diceRoll8)
+        return
+    }
+    if (diceRoll <= 46) {
+        var diceRoll6 = diceRollD6();
+        console.log("Roll " + diceRoll6 + " times on Magic Item Table D")
+        rollOnTableD(diceRoll6)
+        return
+    }
+    if (diceRoll <= 68) {
+        var diceRoll6 = diceRollD6();
+        console.log("Roll " + diceRoll6 + " times on Magic Item Table E")
+        rollOnTableE(diceRoll6)
+        return
+    }
+    if (diceRoll <= 72) {
+        var diceRoll4 = diceRollD4();
+        console.log("Roll " + diceRoll4 + " times on Magic Item Table G")
+        rollOnTableG(diceRoll4)
+        return
+    }
+    if (diceRoll <= 80) {
+        var diceRoll4 = diceRollD4();
+        console.log("Roll " + diceRoll4 + " times on Magic Item Table H")
+        rollOnTableH(diceRoll4)
+        return
+    }
+    console.log("Roll once on Magic Item Table I")
+    rollOnTableI()
+    return
+}
 
-function rollOnTableA(diceRoll6) {
-    var magicItemList = new Array(diceRoll6).fill(null);
+function rollOnTableA(numberOfRolls) {
+    var magicItemList = new Array(numberOfRolls).fill(null);
     return magicItemList.map(function () {
         return genericTable(magicTableAData);
     });
 }
-function rollOnTableB(diceRoll4) {
-    var magicItemList = new Array(diceRoll4).fill(null);
+function rollOnTableB(numberOfRolls) {
+    var magicItemList = new Array(numberOfRolls).fill(null);
     return magicItemList.map(function () {
         return genericTable(magicTableBData);
     });
 }
-function rollOnTableC(diceRoll4) {
-    var magicItemList = new Array(diceRoll4).fill(null);
+function rollOnTableC(numberOfRolls) {
+    var magicItemList = new Array(numberOfRolls).fill(null);
     return magicItemList.map(function () {
         return genericTable(magicTableCData);
     });
 }
-function rollOnTableF(diceRoll4) {
-    var magicItemList = new Array(diceRoll4).fill(null);
+function rollOnTableD(numberOfRolls) {
+    var magicItemList = new Array(numberOfRolls).fill(null);
+    return magicItemList.map(function () {
+        return genericTable(magicTableDData);
+    });
+}
+function rollOnTableE(numberOfRolls) {
+    var magicItemList = new Array(numberOfRolls).fill(null);
+    return magicItemList.map(function () {
+        return genericTable(magicTableEData);
+    });
+}
+function rollOnTableF(numberOfRolls) {
+    var magicItemList = new Array(numberOfRolls).fill(null);
     return magicItemList.map(function () {
         return genericTable(magicTableFData);
     });
 }
-function rollOnTableG() {
-    return genericTable(magicTableGData);
+function rollOnTableG(numberOfRolls) {
+    var magicItemList = new Array(numberOfRolls).fill(null);
+    return magicItemList.map(function () {
+        return genericTable(magicTableGData);
+    });
 }
-function rollOnTableArmor(diceRoll12) {
+function rollOnTableH(numberOfRolls) {
+    var magicItemList = new Array(numberOfRolls).fill(null);
+    return magicItemList.map(function () {
+        return genericTable(magicTableHData);
+    });
+}
+function rollOnTableArmor() {
     var diceRoll12 = diceRollD12();
     var magicItemList = new Array(diceRoll12).fill(null);
     return magicItemList.map(function () {
